7254aa4ca77d7dc2e142a6621b9347a013c35d62
Yi He <yih@yih-thinkpadt14gen4.nay.csb>
2024-11-01 17:39:32 +0800

    c

--- tmt/tests/test.sh
+++ tmt/tests/test.sh
@@ -4,14 +4,16 @@ set -euox pipefail
 cd ../../tests || exit 1
 
 function run_tests() {
-	if [ "$TEST_CASE" = "greenboot-bootc-qcow2" ]; then
+	if [ "$TEST_CASE" = "bootc-qcow2" ]; then
 		./greenboot-bootc-qcow2.sh
-	elif [ "$TEST_CASE" = "greenboot-bootc-anaconda-iso" ]; then
+	elif [ "$TEST_CASE" = "bootc-anaconda-iso" ]; then
 		./greenboot-bootc-anaconda-iso.sh
-	elif [ "$TEST_CASE" = "greenboot-ostree-qcow2" ]; then
+	elif [ "$TEST_CASE" = "ostree-qcow2" ]; then
 		./greenboot-ostree-anaconda-iso.sh
-	elif [ "$TEST_CASE" = "greenboot-ostree-anaconda-iso" ]; then
+	elif [ "$TEST_CASE" = "ostree-anaconda-iso" ]; then
 		./greenboot-ostree-anaconda-iso.sh
+	elif [ "$TEST_CASE" = "bootc-tf" ]; then
+		./greenboot-bootc-tf.sh
 	else
 		echo "Error: Test case $TEST_CASE not found!"
 		exit 1
